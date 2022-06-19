2c3f184ff8caa9e0dbbcaaf13701bd37748d1830
Gareth Jones <Jones258@Gmail.com>
2022-06-20 08:01:01 +1200

    chore: apply prettier & setup ci workflow (#739)
    * refactor: format with prettier
    
    * ci: setup workflow to ensure files are formatted with prettier

--- demo/55.ts
+++ demo/55.ts
@@ -1,12 +1,10 @@
-const {ufs} = require('../src/index');
-const {Volume} = require('memfs');
+const { ufs } = require('../src/index');
+const { Volume } = require('memfs');
 const fs = require('fs');
 
-ufs
-    .use(fs)
-    .use(Volume.fromJSON({"foo.js": ""}, "/tmp"))
+ufs.use(fs).use(Volume.fromJSON({ 'foo.js': '' }, '/tmp'));
 
 console.log(__filename);
 console.log(ufs.existsSync(__filename)); // false
 console.log(fs.existsSync(__filename)); // true
-console.log(ufs.existsSync("/tmp/foo.js")); // true
+console.log(ufs.existsSync('/tmp/foo.js')); // true
